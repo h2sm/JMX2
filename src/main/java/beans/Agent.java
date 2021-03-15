e432864d1ce00180372cbe84c87bed5fb453f177
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-03-15 21:57:21 +0300

    added instrumentation method

--- src/main/java/beans/Agent.java
+++ src/main/java/beans/Agent.java
@@ -4,6 +4,7 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String agentArgs, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new MainTransformer());
     }
 
 }
