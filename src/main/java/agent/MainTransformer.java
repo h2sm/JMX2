b5535eaa277fad4d4b8a8f84c22eae5d51947fb3
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-03-16 13:29:05 +0300

    adding more methods

--- src/main/java/agent/MainTransformer.java
+++ src/main/java/agent/MainTransformer.java
@@ -18,14 +18,14 @@ public class MainTransformer implements ClassFileTransformer {
                 ClassPool pool = ClassPool.getDefault();
                 CtClass clazz = pool.get("TaskManager/run");
 
-                CtField startTiming = new CtField(CtClass.longType,"timeStart",clazz);//создаем long для таймера
-                startTiming.setModifiers(Modifier.STATIC | Modifier.PRIVATE);
-                clazz.addField(startTiming,"0.0");
+                CtField startTiming = CtField.make("private static long timeStart = 0.0", clazz);
+                clazz.addField(startTiming);
 
                 CtField stopTiming = CtField.make("private static long timeStop = 0.0;", clazz);
                 clazz.addField(stopTiming);
 
                 CtField timeElapsed = CtField.make("private static long timeElapsed = 0.0;",clazz);
+                clazz.addField(timeElapsed);
 
                 CtMethod startStuff = CtMethod.make("private static void startStuff(){" +
                         "timeStart = System.nanoTime();" +
@@ -33,23 +33,12 @@ public class MainTransformer implements ClassFileTransformer {
 
                 CtMethod stopStuff = CtMethod.make("private static void stopStuff(){" +
                         "timeStop = System.nanoTime();" +
+                        "timeElapsed = (timeStop - timeStart)/1000000;" +
                         "}",clazz);
 
-                CtMethod countingMethod = CtMethod.make("private static void timeCount(){" +
-                        "timeElapsed = (timeStop - timeStart)/1000000}" +
-                        "", clazz);
-
-//                CtMethod newMethod = CtMethod.make(""
-//                        private static void newMethod() {
-//                            System.out.println("x + y = " + (x + y));
-//                            x += 10;
-//                            y += 20;
-//                            System.out.println("after update: x + y = " + (x + y));
-//                        }
-//                        "", clazz);
-
+                clazz.addMethod(startStuff);
                 clazz.addMethod(stopStuff);
-                clazz.addMethod(countingMethod);
+
                 clazz.getDeclaredMethod("run").insertBefore("startStuff();");//начинаем отсчет
                 clazz.getDeclaredMethod("run").insertAfter("endStuff();");//заканчиваем отчет
                 clazz.getDeclaredMethod("run").insertAfter("timeCount();");
