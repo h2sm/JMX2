531a884876b7caadf897f5ce331bcc7299e6aaf9
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-03-17 21:00:50 +0300

    well it doesnt work either

--- src/main/java/agent/MainTransformer.java
+++ src/main/java/agent/MainTransformer.java
@@ -5,22 +5,26 @@ import runenvironment.TaskManager;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 
 public class MainTransformer implements ClassFileTransformer,MainTransformerMBean {
+    private final String path = "D:\\Java_laba\\TestHelloWorld\\build\\classes\\java\\main";
     @Override
     public byte[] transform(ClassLoader loader,
                             String className,
                             Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain,
                             byte[] classfileBuffer) throws IllegalClassFormatException {
-        //System.out.println("classname " + className);
+        System.out.println("classname " + className);
 
         try {
-            if (className.equals("main/Main")) {//если загружаемый класс был передан через jmx
+            if (className.equals("Main")) {//если загружаемый класс был передан через jmx
+                System.out.println("ok " + className);
                 ClassPool pool = ClassPool.getDefault();
-                CtClass clazz = pool.get("main.Main");
+                pool.insertClassPath(path);
+                CtClass clazz = pool.get("Main");
 
-                CtField startTiming = CtField.make("private static long timeStart = 0.0", clazz);
+                CtField startTiming = CtField.make("private static long timeStart = 0.0;", clazz);
                 clazz.addField(startTiming);
 
                 CtField stopTiming = CtField.make("private static long timeStop = 0.0;", clazz);
@@ -36,15 +40,14 @@ public class MainTransformer implements ClassFileTransformer,MainTransformerMBea
                 CtMethod stopStuff = CtMethod.make("private static void stopStuff(){" +
                         "timeStop = System.nanoTime();" +
                         "timeElapsed = (timeStop - timeStart)/1000000;" +
-                        "System.out.println(\"[profile] task \" + className + \" time elapsed : \" + timeElapsed);\n" +
+                        "System.out.println(\"[profile] task \" + \" time elapsed : \" + timeElapsed);\n" +
                         "}",clazz);
                 clazz.addMethod(startStuff);
                 clazz.addMethod(stopStuff);
-                clazz.getDeclaredMethod("main");
 
                 clazz.getDeclaredMethod("main").insertBefore("startStuff();");
                 clazz.getDeclaredMethod("main").insertAfter("stopStuff();");
-
+                clazz.writeFile("D:\\Java_laba");
                 return clazz.toBytecode();
             }
             else {
