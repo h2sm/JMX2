c6f3cffbd1cbdc1a15b031140276c7ec6c784358
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-03-22 20:59:48 +0300

    profiling works

--- src/main/java/agent/MainTransformer.java
+++ src/main/java/agent/MainTransformer.java
@@ -3,13 +3,13 @@ import javassist.*;
 import javassist.bytecode.Descriptor;
 import runenvironment.TaskManager;
 
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 
 public class MainTransformer implements ClassFileTransformer {
-    private final String path = "D:\\Java_laba\\TestHelloWorld\\build\\classes\\java\\main";
 
     @Override
     public byte[] transform(ClassLoader loader,
@@ -21,43 +21,23 @@ public class MainTransformer implements ClassFileTransformer {
 
         try {
             if (className.equals("Main")) {//если загружаемый класс был передан через jmx
-                System.out.println("ok " + className);
-//                ClassPool pool = ClassPool.getDefault();
-//                pool.insertClassPath(path);
-//                CtClass clazz = pool.get("Main");
-//
-//                CtField startTiming = CtField.make("private static double timeStart = System.currentTimeMillis();", clazz);
-//                clazz.addField(startTiming);
-//
-//                CtField stopTiming = CtField.make("private static double timeStop = 0.0;", clazz);
-//                clazz.addField(stopTiming);
-//
-//                CtField timeElapsed = CtField.make("private static double timeElapsed = 0.0;",clazz);
-//                clazz.addField(timeElapsed);
-//
-//                CtMethod stopStuff = CtMethod.make("private static void stopStuff(){" +
-//                        "timeStop = System.currentTimeMillis();" +
-//                        "timeElapsed = timeStop - timeStart;" +
-//                        "System.out.println(\"[profile] task \" + \" time elapsed : \" + timeElapsed);\n" +
-//                        "}",clazz);
-//
-//                clazz.addMethod(stopStuff);
-//
-//                clazz.getDeclaredMethod("main").insertAfter("stopStuff();");
-//                clazz.writeFile("D:\\Java_laba");
-//                clazz.detach();
-//                return clazz.toBytecode();
-
                 ClassPool classPool = ClassPool.getDefault();
-                classPool.insertClassPath(path);
+                classPool.appendClassPath(new LoaderClassPath(loader));
+                CtClass cc = classPool.get(className);
+                CtMethod[] methods = cc.getMethods();
 
-                CtMethod method = classPool
-                        .getMethod(Descriptor.toJavaName(className),"main");
-                method.insertAfter("System.out.println " +
-                        "(\"...hello.\");");
-                byte[] newClass = method.getDeclaringClass().toBytecode();
-                method.getDeclaringClass().detach();
-                return newClass;
+                for( CtMethod method : methods){
+                    if (method.getName().equals("main")){
+                        System.out.println("Entering "+method.getName() + " of " + className);
+                        cc.defrost();
+                        method.addLocalVariable("elapsedTime", CtClass.longType);
+                        method.insertBefore("elapsedTime = System.currentTimeMillis();");
+                        method.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
+                                + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+                    }
+                }
+                return cc.toBytecode();
+
             }
             else {
                 return classfileBuffer;
